7a4ff83d12c820e86c5bb4a9188a4edc7aaa87dc
igorlbraga <hakcof@outlook.com>
2025-07-09 11:27:50 -0300

    feat: add tracking

--- src/app/coming-soon/page.tsx
+++ src/app/coming-soon/page.tsx
@@ -1,11 +1,8 @@
-import { redirect } from "next/navigation";
-
 export default function Home() {
-  redirect(process.env.REDIRECT_URL || "/coming-soon")
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Já ouviu falar em Phishing?
+        Ainda em desenvolvimento...
       </main>
     </div>
   );
